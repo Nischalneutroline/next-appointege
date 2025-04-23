d0256ce362d1eeadf5cc8cf988b78ed799f2b590
Nischal Puri <nischal1.neutroline@gmail.com>
2025-04-23 18:32:05 +0545

    all form done

--- src/app/(admin)/appointment/page.tsx
+++ src/app/(admin)/appointment/page.tsx
@@ -10,8 +10,8 @@ const AppointmentFormPage = () => {
     <main className="h-full flex flex-col">
       <PageHeader>
         <Heading
-          title="Create New Appointment"
-          description="Schedule a new appointment"
+          title="Create New Customer"
+          description="Create and Mange your customers"
           icon={<CalendarDays />}
         />
       </PageHeader>
