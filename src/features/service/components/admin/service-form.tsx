9e1b715b13ee98598d1d3e58c6906cb2bec94f63
Nischal Puri <nischal1.neutroline@gmail.com>
2025-04-24 12:03:48 +0545

    all logic buid for service time too

--- src/features/service/components/admin/service-form.tsx
+++ src/features/service/components/admin/service-form.tsx
@@ -12,28 +12,25 @@ import ServiceHoursSelector from "@/components/custom-form-fields/serivce/servic
 import ToggleSwitch from "@/components/custom-form-fields/toggle-switch"
 import DurationSelect from "@/components/custom-form-fields/duration-select"
 import { CalendarClock, ImageUp, ScrollText, UserRoundCog } from "lucide-react"
+import { Toaster } from "sonner"
 
-// For Edit Form this has to be the data structure to pass in the form
-const fetchedService = {
-  serviceName: "Haircut",
-  description: "A fresh new look",
-  image: null,
-  availabilityMode: "custom",
-  serviceDays: ["Mon", "Wed", "Fri"],
-  serviceHours: {
-    Mon: [
-      ["09:00 AM", "12:00 PM"],
-      ["01:00 PM", "03:00 PM"],
-    ],
-    Wed: [["10:00 AM", "02:00 PM"]],
-    Fri: [
-      ["09:00 AM", "11:00 AM"],
-      ["12:00 PM", "04:00 PM"],
+// Business availability data
+export type WeekDay = "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun"
+export const businessAvailability = {
+  Mon: {
+    break: [
+      ["12:00 PM", "01:00 PM"],
+      ["02:00 PM", "03:00 PM"],
+      ["04:00 PM", "05:00 PM"],
     ],
   },
-  isAvailable: true,
-  duration: "60", // in minutes
-}
+  Tue: { break: [["02:00 PM", "04:00 PM"]] },
+  Wed: { break: [] },
+  Thu: { break: [] },
+  Fri: { break: [] },
+  Sat: { break: [] },
+  Sun: { break: [] },
+} satisfies Record<WeekDay, { break: [string, string][] }>
 
 const defaultServiceHours = {
   Mon: [["09:00 AM", "05:00 PM"]],
@@ -46,23 +43,18 @@ const defaultServiceHours = {
 }
 
 export default function ServiceForm() {
-  // for edit form
   const form = useForm({
-    defaultValues: fetchedService,
+    defaultValues: {
+      serviceName: "",
+      description: "",
+      image: null,
+      availabilityMode: "default",
+      serviceDays: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+      serviceHours: defaultServiceHours,
+      isAvailable: true,
+      duration: "",
+    },
   })
-
-  // const form = useForm({
-  //   defaultValues: {
-  //     serviceName: "",
-  //     description: "",
-  //     image: null,
-  //     availabilityMode: "default",
-  //     serviceDays: ["Mon", "Tue", "Wed", "Thu", "Fri"],
-  //     serviceHours: defaultServiceHours,
-  //     isAvailable: true,
-  //     duration: "",
-  //   },
-  // })
 
   const onSubmit = (data: any) => {
     console.log("Submitted Data:", data)
@@ -70,9 +62,10 @@ export default function ServiceForm() {
 
   return (
     <FormProvider {...form}>
+      <Toaster position="top-right" />
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <div className="">
-          <div className=" space-y-6">
+          <div className="space-y-6">
             <InputField
               name="serviceName"
               label="Service Name"
@@ -90,8 +83,11 @@ export default function ServiceForm() {
             />
             <AvailabilityTabs name="availabilityMode" icon={CalendarClock} />
             <ServiceDaySelector name="serviceDays" />
-            <ServiceHoursSelector name="serviceHours" />
-            <div className="flex flex-col md:flex-row gap-4  justify-between">
+            <ServiceHoursSelector
+              name="serviceHours"
+              businessBreaks={businessAvailability}
+            />
+            <div className="flex flex-col md:flex-row gap-4 justify-between">
               <ToggleSwitch name="isAvailable" label="Availability" />
               <DurationSelect name="duration" label="Duration:" />
             </div>
